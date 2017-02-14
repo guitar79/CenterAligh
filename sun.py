98aaddf5f8d439ba0a4f42b853bfdf1032cf1f07
Kiehyun Kevin Park <guitar79@naver.com>
2017-02-15 03:43:12 +0900

    Create sun.py

---
+++ sun.py
@@ -0,0 +1,17 @@
+import os, sys
+from PIL import Image, ImageOps
+
+def thresh1(x): #zero threshold for finding sunspots
+ if x < 60:
+ x = 0
+ return x
+
+def thresh2(x): #zero threshold for cropping around sun
+ if x < 128:
+ x = 0
+ return x
+
+def bbcrop(image, border):
+ box1 = image.point(thresh2).getbbox()
+ box2 = (box1[0]-border, box1[1]-border, box1[2]+border, box1[3]+border)
+ return image.crop(box2)
