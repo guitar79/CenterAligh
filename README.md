1669e1f72b938eca292e6f8974178fedc2246f46
Seungwon Park <psw14041@gmail.com>
2017-04-02 00:00:16 +0900

    Made error log.txt

--- README.md
+++ README.md
@@ -6,5 +6,10 @@ Demo of [Aligning sun images using python](https://labjg.wordpress.com/2013/04/0
 
 - Create `img` directory.
 - Create `in`, `out` in `img` directory.
-- Place your sun files at 'img/in/'.
+- Place your sun files at `img/in/`.
 - Run `python2 suncrop.py`.
+
+## Results
+
+- Processed images are saved to `img/out/`.
+- List of images that made an error are saved to `errorlog.txt`.
--- suncrop.py
+++ suncrop.py
@@ -3,6 +3,7 @@ from PIL import Image
 
 input_image_list = os.listdir('img/in/')
 
+f = open('errorlog.txt','a')
 for filename in input_image_list:
 	if(filename[-4:] != '.JPG'): # skip file if it's not image file
 		continue
@@ -10,13 +11,21 @@ for filename in input_image_list:
 	infile = 'img/in/' + filename
 	outfile = 'img/out/' + filename
 
-	im = Image.open(infile)
-	print "\nFile %s (%s, %s, %d x %d)" % (infile, im.format, im.mode, im.size[0], im.size[1])
+	if(os.path.isfile(outfile)):
+		continue
+	
+	try:
+		im = Image.open(infile)
+		print "\nFile %s (%s, %s, %d x %d)" % (infile, im.format, im.mode, im.size[0], im.size[1])
 
-	print "Finding sun..."
-	imtrim = sun.bbcrop(im, 200)
+		print "Finding sun..."
+		imtrim = sun.bbcrop(im, 200)
 
-	print "Saving to %s" % outfile
-	imtrim.save(outfile)
+		print "Saving to %s" % outfile
+		imtrim.save(outfile)
+	except:
+		print "Error occured while processing %s" % (infile)
+		f.write('%s\n' % (infile))
+
 
 print "Done!\n"
