5505fbd0241ec24123f75868c9ce6c8e4a849bbd
Seungwon Park <psw14041@gmail.com>
2017-04-01 23:13:29 +0900

    Read image list from list.txt

--- suncrop.py
+++ suncrop.py
@@ -1,10 +1,12 @@
 import os, sys, sun
 from PIL import Image
 
-for i in range (1,400):		#image index range
+input_image_list = [x.replace('\n','') for x in open('list.txt').readlines()]
 
-	infile = "img/in/sun-%04d.tif" % i
-	outfile = "img/crop/sun-%04d.tif" % i
+for filename in input_image_list:
+
+	infile = 'img/in/' + filename
+	outfile = 'img/out/' + filename
 
 	im = Image.open(infile)
 	print "\nFile %s (%s, %s, %d x %d)" % (infile, im.format, im.mode, im.size[0], im.size[1])
